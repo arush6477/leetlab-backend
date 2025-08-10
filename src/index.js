f4c401c7a60d0af8094aa1f701c8dae30501c278
Arush Choudhary <arushgzbin@gmail.com>
2025-08-10 13:20:14 +0530

    cors bhi update kia ha abhi

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173", "https://extraordinary-treacle-a1e9f1.netlify.app"],
     credentials:true
 }))
 
